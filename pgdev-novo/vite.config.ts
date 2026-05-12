b77e9a5d04e2c563d9755ddbaa1296e399514e79
xwhamlnieha <5983077+xwhamlnieha@users.noreply.github.com>
2026-05-12 10:42:42 -0300

    update

--- pgdev-novo/vite.config.ts
+++ pgdev-novo/vite.config.ts
@@ -1,6 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'PabloG.Dev',
+        short_name: 'PabloG.Dev',
+        theme_color: '#000c24',
+        background_color: '#000c24',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/pwa-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
